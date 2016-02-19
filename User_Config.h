b20db226a9ffeb652d6a303b10285c86861834fb
Bodmer <bodmer@anola.net>
2016-02-19 00:16:43 +0000

    Correct typo

--- User_Config.h
+++ User_Config.h
@@ -1,4 +1,4 @@
-// Comment out the line below is you are not using an SD Card to store the JPEGs
+// Comment out the line below if you are not using an SD Card to store the JPEGs
 // Commenting out the line is NOT essential but will save some FLASH space if
 // SD Card access is not needed.
 
